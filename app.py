8f3d70d62024b9f2022c933338c50244c16db18a
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-12 15:26:41 -0300

    feat: first-lesson

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Minha Primeira página</h1>'
+
+@app.route('/segunda-pagina')
+def segunda_pagina():
+    return '<h1>Minha Segunda página</h1>'
+
+@app.route('/form')
+def form():
+    return '''<form>
+                <label>Primeiro nome:</label>
+                <input type="text" placeholder="digite o seu primeiro nome"><br><br>\
+                <label>Segundo  Nome:</label>
+                <input type="text" placeholder="digite o seu segundo nome"><br><br>\
+                <input type="submit" value="enviar">
+            </form>'''
+
+
 app.run()
 
 # O comando "set FLASK_APP=app" é usado para definir a variável de ambiente FLASK_APP
