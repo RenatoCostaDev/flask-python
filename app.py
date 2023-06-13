756cfb529c831ef8af09d599fb73852d91d07fee
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-13 10:07:15 -0300

    feat: cleanning app.py

--- app.py
+++ app.py
@@ -25,14 +25,4 @@ def contatos():
 
 app.run()
 
-# methods=['GET'] is a default method.
-# @app.route('/v1/users/idade/<nome>', methods=['GET']) 
-# def retorna_idade(nome: str):
-#     usuarios_filtrados = filter(lambda x: x["nome"] == nome.capitalize(), data["users"])
-#     idade = next(usuarios_filtrados, {"idade": None})["idade"]
-
-#     if nome:
-#          return { 'idade': idade }
-#     else:
-#     	return { 'idade': None }
 
