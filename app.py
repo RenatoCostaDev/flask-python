8f2ae1bcc0b58ec194151a3bd601a9de1e2fc816
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-13 16:18:03 -0300

    feat: index with render jinja2 beinng used

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
+links = [
+    {'nome': 'linkedin', 'url': 'https://www.linkedin.com/in/renatocostadev/'},
+    {'nome': 'gitHub', 'url': 'https://github.com/RenatoCostaDev'},
+]
+
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
     return render_template(
-         'index.html'
+         'index.html',
+         links=links
     )
 
 app.run(debug=True)
