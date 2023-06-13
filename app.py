5e0f5104737e3acf72bf437a7b80ad1dc09f83e2
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-13 10:03:33 -0300

    feat: creating contatos.html and some new routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 data = {
 	"users" : [
@@ -15,15 +15,24 @@ data = {
 
 app = Flask(__name__)
 
-# methods=['GET'] is a default method.
-@app.route('/v1/users/idade/<nome>', methods=['GET']) 
-def retorna_idade(nome: str):
-    usuarios_filtrados = filter(lambda x: x["nome"] == nome.capitalize(), data["users"])
-    idade = next(usuarios_filtrados, {"idade": None})["idade"]
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
-    if nome:
-         return { 'idade': idade }
-    else:
-    	return { 'idade': None }
+@app.route("/contatos")
+def contatos():
+    return render_template('contatos.html')
 
 app.run()
+
+# methods=['GET'] is a default method.
+# @app.route('/v1/users/idade/<nome>', methods=['GET']) 
+# def retorna_idade(nome: str):
+#     usuarios_filtrados = filter(lambda x: x["nome"] == nome.capitalize(), data["users"])
+#     idade = next(usuarios_filtrados, {"idade": None})["idade"]
+
+#     if nome:
+#          return { 'idade': idade }
+#     else:
+#     	return { 'idade': None }
+
