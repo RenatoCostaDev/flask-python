faa9a3af236fc87c5648a49b1699b271d0a91278
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-13 16:00:44 -0300

    feat: initial base

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-from flask import Flask
-
-data = {
-	"users" : [
-        {
-            "nome": "Joao",
-            "idade": 24
-        },
-        {
-            "nome": "Francisco",
-            "idade": 27
-        }
-    ]
-}
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# methods=['GET'] is a default method.
-@app.route('/v1/users/idade/<nome>', methods=['GET']) 
-def retorna_idade(nome: str):
-    usuarios_filtrados = filter(lambda x: x["nome"] == nome.capitalize(), data["users"])
-    idade = next(usuarios_filtrados, {"idade": None})["idade"]
+@app.route("/index")
+def index():
+    return render_template(
+         'index.html'
+    )
 
-    if nome:
-         return { 'idade': idade }
-    else:
-    	return { 'idade': None }
-
-app.run()
+app.run(debug=True)
