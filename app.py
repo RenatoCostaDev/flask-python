e02180e515134584c0f93cb169cf9072a8607228
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-14 14:16:51 -0300

    feat: first part of the lesson

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 
-data = {
-	"users" : [
-        {
-            "nome": "Joao",
-            "idade": 24
-        },
-        {
-            "nome": "Francisco",
-            "idade": 27
-        }
-    ]
-}
+contatos = [
+    {
+ 	    "nome" : "fulano",
+        "email" : "fulano@email.com",
+        "celular" : "00000000",
+        "tags" : ["trabalho"], #lista 
+        'links_img_tag' : ['https://…..png'] #lista
+    }
+]
 
 app = Flask(__name__)
 
-# methods=['GET'] is a default method.
-@app.route('/v1/users/idade/<nome>', methods=['GET']) 
-def retorna_idade(nome: str):
-    usuarios_filtrados = filter(lambda x: x["nome"] == nome.capitalize(), data["users"])
-    idade = next(usuarios_filtrados, {"idade": None})["idade"]
+@app.route("/contatos")
+def contatos():
+    return render_template(
+        'contatos.html',
+        contatos=contatos
+    )
 
-    if nome:
-         return { 'idade': idade }
-    else:
-    	return { 'idade': None }
-
-app.run()
+app.run(debug=True)
