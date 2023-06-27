9a5348e9da227f7fca1747ea4748892fe5c68c92
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-27 10:41:09 -0300

    feat: delete route done

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 contatos_list = [
     {
  	    "nome" : "flask",
         "email" : "flask@email.com",
         "celular" : "00000000",
-        "tags" : ["Flask", "Flask_2"], #lista 
-        'links_img_tag' : ['https://flask.palletsprojects.com/en/2.3.x/'] #lista
     },
     {
  	    "nome" : "python",
         "email" : "python@email.com",
         "celular" : "00000000",
-        "tags" : ["Python", "Python_2"], #lista 
-        'links_img_tag' : ['https://www.python.org/'] #lista
     },
 ]
 
@@ -32,10 +28,28 @@ def contatos():
 #  you will need to pass the same way you pass
 #  other variables to be used in your jinja template.
 
-@app.route("/adicionar-contato")
+@app.route("/adicionar-contato", methods=['GET', 'POST'])
 def contatos_form():
+    if request.method == 'POST':
+        if request.form.get('nome') and request.form.get('celular') and request.form.get('email'):
+            contatos_list.append({
+               'nome': request.form.get('nome'),
+               'celular': request.form.get('celular'),
+               'email': request.form.get('email'),
+            })
+            return redirect('/contatos')
     return render_template(
         'cadastro.html',
     )
+
+@app.route("/<email>/deletar_contato", methods=['GET', 'POST'])
+def deletar_contato(email):
+    # email_recebido = request.form['email']
+    for i in range(len(contatos_list)):
+        if contatos_list[i]['email'] == str(email): #Verifica o email recebido
+            contatos_list.pop(i)
+        return redirect('/contatos')
+    app.run()
+
 
 app.run(debug=True)
