ea4d9aa8ddeaef6965f227811f5fe33a38e60af7
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-29 09:45:04 -0300

    feat: utils.py done

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 
 def get_contact_by_email(email, contatos_list):
-        for contato in contatos_list:
-            if contato["email"] == email:
-                return contato
-        return None
+    for contato in contatos_list:
+        if contato["email"] == email:
+            return contato
+    return None
